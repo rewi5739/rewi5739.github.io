aacedd6564f59063813c41cef5f214283305052d
Regan Wilson <regan.wilson-1@colorado.edu>
2025-04-26 12:50:38 -0600

    functional, pretty sudoku

--- final/script.js
+++ final/script.js
@@ -1,17 +1,37 @@
+
+let oldprobchild;
+    
 function checkSudoku() {
     const article = document.querySelector("article");
     const boxes = article.children;
     const rows = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
     const columns = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
-    validSudoku = true;
+    let validSudoku = true;
+    let sudokuError = "";
+
+    console.log(oldprobchild);
+    if(oldprobchild) {
+        oldprobchild.style.backgroundColor = "rgb(223, 240, 227)";
+        if(oldprobchild.className.includes("entry")){
+            oldprobchild.style.backgroundColor = "rgb(93, 235, 58)";
+        }
+        oldprobchild = undefined;
+    }
 
     for(const box of boxes){
         validSudoku = validSudoku && check9(box.children);
     }
+    if(!validSudoku && sudokuError == ""){
+        sudokuError = "box"
+    }
+
     for(const rowName of rows){
         const row = document.querySelectorAll("."+rowName);
         validSudoku = validSudoku && check9(row);
         // console.log(row);
+    }
+    if(!validSudoku && sudokuError == ""){
+        sudokuError = "row"
     }
 
     for(const colName of columns){
@@ -19,10 +39,16 @@ function checkSudoku() {
         validSudoku = validSudoku && check9(col);
         // console.log(col);
     }
+    if(!validSudoku && sudokuError == ""){
+        sudokuError = "collumn"
+    }
 
-    // let hopefulBool = check9(children);
-
-    console.log("Valid Sudoku = " + validSudoku);
+    if (validSudoku){
+        console.log("Valid Sudoku");
+        submitNumber();
+    } else {
+        alert("Invalid Sudoku. Reason: " + sudokuError)
+    }
 }
 
 function check9(elementList){
@@ -56,9 +82,21 @@ function check9(elementList){
         }
     }
 
-    // if (values.length != 9){
-    //     returnVal = false;
-    // }
+    if (problemChild){
+        problemChild.style.backgroundColor = "red";
+        oldprobchild = problemChild;
+    }
 
     return returnVal;
+}
+
+function submitNumber() {
+    let phoneNumberStr = "";
+    const entryBoxes = document.querySelectorAll(".entry");
+    console.log(entryBoxes)
+    for(entry of entryBoxes){
+        phoneNumberStr += entry.value;
+    }
+    let phoneNumber = "(" + phoneNumberStr.slice(0,3) + ")" + phoneNumberStr.slice(3, 6) + "-" + phoneNumberStr.slice(6);
+    console.log(phoneNumber);
 }
